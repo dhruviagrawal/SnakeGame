97c7d82703dff391a63bdc4e4c42848465747a0d
Dhruvi Agrawal <dhruviagrawal2@gmail.com>
2023-07-21 22:57:01 +0530

    keep track of high score

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
         super().__init__()
         self.color("white")
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
@@ -26,6 +27,8 @@ class Scoreboard(Turtle):
     def rest(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
