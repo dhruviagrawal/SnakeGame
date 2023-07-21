991cf358cfb9202683df7142e919996f45e5dfe2
Dhruvi Agrawal <dhruviagrawal2@gmail.com>
2023-07-21 22:27:21 +0530

    Keeping high score and reset game

--- scoreboard.py
+++ scoreboard.py
@@ -9,21 +9,28 @@ class Scoreboard(Turtle):
         super().__init__()
         self.color("white")
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
+
+    def rest(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def inc_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
 
